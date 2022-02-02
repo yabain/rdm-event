4d2297c601714d6e5ddc9f90c33e809c936f8e08
Flambel SANOU <flambel55@gmail.com>
2022-02-02 23:54:39 +0100

    top header

--- src/app/shared/components/top-header-menu/top-header-menu.component.ts
+++ src/app/shared/components/top-header-menu/top-header-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-top-header-menu',
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./top-header-menu.component.scss']
 })
 export class TopHeaderMenuComponent implements OnInit {
+  @Input()
+  isLogin = false;
+  isAdmin = true;
 
-  constructor() { }
+  constructor(
+  ) { 
+  }
 
   ngOnInit(): void {
   }
-
 }
