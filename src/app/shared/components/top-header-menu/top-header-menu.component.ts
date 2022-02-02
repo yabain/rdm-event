6c6bc521cec9c8da108384a55d4de05597f716a4
Cedric-Yaba-In <c.nguendap@yabain.com>
2022-02-03 00:25:06 +0100

    handle bug on show connexion label on top-header-menu component

--- src/app/shared/components/top-header-menu/top-header-menu.component.ts
+++ src/app/shared/components/top-header-menu/top-header-menu.component.ts
@@ -7,8 +7,8 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class TopHeaderMenuComponent implements OnInit {
   @Input()
-  isLogin = false;
-  isAdmin = true;
+  isLogin:boolean = false;
+  isAdmin:boolean = true;
 
   constructor(
   ) { 
