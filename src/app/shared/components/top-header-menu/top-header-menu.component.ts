4a9da16d7a395579f4515b1b505982ee0af20d60
Cedric-Yaba-In <c.nguendap@yabain.com>
2022-02-11 14:56:43 +0100

    show name to coneected user in top header bar

--- src/app/shared/components/top-header-menu/top-header-menu.component.ts
+++ src/app/shared/components/top-header-menu/top-header-menu.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { User } from '../../entities';
 import { AuthService } from '../../services/auth/auth.service';
+import { UserProfilService } from '../../services/user-profil/user-profil.service';
 
 @Component({
   selector: 'app-top-header-menu',
@@ -11,15 +13,25 @@ export class TopHeaderMenuComponent implements OnInit {
   isAuth:boolean = false;
   isOwner:boolean = false;
   isAdmin:boolean = false;
+  user:User=new User()
 
   constructor(
-    private isAuthService: AuthService
+    private authService: AuthService,
+    private userProfilService:UserProfilService
   ) { 
-    this.isAuth = this.isAuthService.isAuth;
-    // this.isOwner = true;
-    this.isAdmin = this.isAuthService.isAdmin;
+    // this.isAuth = this.isAuthService.isAuth;
+    // // this.isOwner = true;
+    // this.isAdmin = this.isAuthService.isAdmin;
+   
   }
 
   ngOnInit(): void {
+    this.userProfilService.currentUser.subscribe((user:User)=>{
+      this.user=user;
+    })
+    this.authService.isLoggedIn.subscribe((isLoggin)=>{
+      this.isAuth=isLoggin
+    })
+
   }
 }
